b8c61687acf353620b82c2146a51baffe9074888
Peter <mendez.pedro94@gmail.com>
2023-08-29 17:22:53 -0400

    create seed for db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Store.destroy_all
+DeviceServer.destroy_all
+
+2.times do |x|
+    Store.create(
+        name: "Restaurant #{x + 1}",
+        status: 0)
+end
+
+devices_server = ["Impresora", "servidor web", "servidor de base de datos"]
+Store.all.each do |store|
+    devices_server.each do |devices|
+        DeviceServer.create(
+            name: devices,
+            status: 0,
+            store_id: store.id
+        )
+    end
+end
