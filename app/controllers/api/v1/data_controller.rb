3de9c7c8a78dffea7bc550041af8ec702259e574
Peter <mendez.pedro94@gmail.com>
2023-08-31 00:10:31 -0400

    fix json of endpoint

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -5,11 +5,11 @@ module Api
       def status_report
         options = [200, 404]
         data = {
-          impesora_1: options[rand(options.length)],
-          servidor_web_1: options[rand(options.length)],
+          Impresora_1: options[rand(options.length)],
+          Servidor_web_1: options[rand(options.length)],
           Servidor_de_base_de_datos_1: options[rand(options.length)],
-          impesora_2: options[rand(options.length)],
-          servidor_web_2: options[rand(options.length)],
+          Impresora_2: options[rand(options.length)],
+          Servidor_web_2: options[rand(options.length)],
           Servidor_de_base_de_datos_2: options[rand(options.length)]
         }
         render json: data
