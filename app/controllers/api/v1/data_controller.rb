ecdb51744af2c7a0e9de9e533417ce7cdb9d997b
Peter <mendez.pedro94@gmail.com>
2023-08-30 20:50:21 -0400

    endpoint status

--- app/controllers/api/v1/data_controller.rb
+++ app/controllers/api/v1/data_controller.rb
@@ -3,14 +3,16 @@ module Api
     class DataController < BaseController
 
       def status_report
-        unless params[:user].present?
-          render json: { mensaje: 'Error de usuario' }, status: :bad_request
-          return 
-        end
-        report = params
-        puts report
-
-        render json: { mensaje: 'Reporte recibido con éxito' }, status: :ok
+        options = [200, 404]
+        data = {
+          impesora_1: options[rand(options.length)],
+          servidor_web_1: options[rand(options.length)],
+          Servidor_de_base_de_datos_1: options[rand(options.length)],
+          impesora_2: options[rand(options.length)],
+          servidor_web_2: options[rand(options.length)],
+          Servidor_de_base_de_datos_2: options[rand(options.length)]
+        }
+        render json: data
       end
     end
   end
